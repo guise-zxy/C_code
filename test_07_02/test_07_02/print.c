0a98bd6f556c72ba8d44dc6b09f8aa080b10ff66
giuse <genius200612zhxy@qq.com>
2025-07-22 21:39:09 +0800

    测试github提交代码

---
+++ test_07_02/test_07_02/print.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+	char a[] = { '0','1','4','6' ,'\0'};
+		printf("%d\n",strlen(a))
+		printf("arr[]",llf)
+	
+	
+
+return 0;
+}
