65d2820fb618a7f99b7bdb2811edb6d1ab3776bb
guise-zxy <genius200612zhxy@qq.com>
2026-05-20 20:44:35 +0800

    Add files via upload

--- 实验七/实验七/C题.cpp
+++ 实验七/实验七/C题.cpp
@@ -1,1 +1,161 @@
-#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS 
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class phone
+{
+private:
+	string a;
+
+public:
+	phone()
+	{
+		this->a = new char[7];
+	}
+
+	phone(const phone& p)
+	{
+		if (p.a[0] == '2' || p.a[0] == '3' || p.a[0] == '4')
+		{
+			'8' += p.a;
+		}
+		else if (p.a[0] == '5' || p.a[0] == '6' || p.a[0] == '7' || p.a[0] == '8')
+		{
+			'2' += p.a;
+		}
+	}
+
+	~phone()
+	{
+		delete[]a;
+	}
+	
+	print()
+	{
+		cout << a;
+	}
+
+
+};
+
+
+int main()
+{
+	int t = 0;
+	cin >> t;
+	while (t--)
+	{
+		string temp;
+		cin >> temp;
+		if(temp[0]=='')
+		phone obj;
+		cin >> obj.a;
+	}
+
+
+
+	return 0;
+}
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class CTelNumber {
+private:
+    char* number;
+
+public:
+    CTelNumber(char* num);
+    CTelNumber(const CTelNumber& t);
+    ~CTelNumber();
+
+    void print();
+};
+
+// 构造函数：保存原来的7位号码
+CTelNumber::CTelNumber(char* num) {
+    int len = strlen(num);
+
+    // 判断长度是否为7
+    if (len != 7) {
+        number = NULL;
+        return;
+    }
+
+    // 判断是否全是数字
+    for (int i = 0; i < len; i++) {
+        if (num[i] < '0' || num[i] > '9') {
+            number = NULL;
+            return;
+        }
+    }
+
+    // 判断开头是否合法：只能是2~8
+    if (num[0] < '2' || num[0] > '8') {
+        number = NULL;
+        return;
+    }
+
+    number = new char[8];   // 7位号码 + '\0'
+    strcpy(number, num);
+}
+
+// 拷贝构造函数：把7位号码升级成8位号码
+CTelNumber::CTelNumber(const CTelNumber& t) {
+    if (t.number == NULL) {
+        number = NULL;
+        return;
+    }
+
+    number = new char[9];   // 8位号码 + '\0'
+
+    if (t.number[0] >= '2' && t.number[0] <= '4') {
+        number[0] = '8';
+    }
+    else if (t.number[0] >= '5' && t.number[0] <= '8') {
+        number[0] = '2';
+    }
+    else {
+        number = NULL;
+        return;
+    }
+
+    strcpy(number + 1, t.number);
+}
+
+// 析构函数
+CTelNumber::~CTelNumber() {
+    if (number != NULL) {
+        delete[] number;
+    }
+}
+
+// 输出
+void CTelNumber::print() {
+    if (number == NULL) {
+        cout << "Illegal phone number" << endl;
+    }
+    else {
+        cout << number << endl;
+    }
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        char num[100];
+        cin >> num;
+
+        CTelNumber oldTel(num);      // 构造原7位号码
+        CTelNumber newTel(oldTel);   // 拷贝构造，升位成8位号码
+
+        newTel.print();
+    }
+
+    return 0;
+}
--- 实验七/实验七/D题目.cpp
+++ 实验七/实验七/D题目.cpp
@@ -1,1 +1,173 @@
-#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class CDate
+{
+private:
+    int year, month, day;
+
+public:
+    CDate(int y, int m, int d);
+    bool isLeapYear();
+    int getYear();
+    int getMonth();
+    int getDay();
+    int getDayofYear();
+};
+
+CDate::CDate(int y, int m, int d)
+{
+    year = y;
+    month = m;
+    day = d;
+}
+
+bool CDate::isLeapYear()
+{
+    return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+}
+
+int CDate::getYear()
+{
+    return year;
+}
+
+int CDate::getMonth()
+{
+    return month;
+}
+
+int CDate::getDay()
+{
+    return day;
+}
+
+int CDate::getDayofYear()
+{
+    int sum = day;
+    int a[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+    if (isLeapYear())
+        a[2]++;
+
+    for (int i = 0; i < month; i++)
+        sum += a[i];
+
+    return sum;
+}
+
+class Software
+{
+private:
+    char name[31];
+    char type;
+    char media;
+    CDate endDate;
+
+public:
+    Software(char n[], char t, char m, int y, int mon, int d);
+    Software(const Software& s);
+
+    int getRemainDays();
+    void print();
+};
+
+Software::Software(char n[], char t, char m, int y, int mon, int d)
+    : endDate(y, mon, d)
+{
+    strcpy(name, n);
+    type = t;
+    media = m;
+}
+
+Software::Software(const Software& s)
+    : endDate(s.endDate)
+{
+    strcpy(name, s.name);
+    type = 'B';
+    media = 'H';
+}
+
+int Software::getRemainDays()
+{
+    int y = endDate.getYear();
+    int m = endDate.getMonth();
+    int d = endDate.getDay();
+
+    int days1 = 365 * (2015 - 1)
+        + (2015 - 1) / 4
+        - (2015 - 1) / 100
+        + (2015 - 1) / 400
+        + CDate(2015, 4, 7).getDayofYear();
+
+    int days2 = 365 * (y - 1)
+        + (y - 1) / 4
+        - (y - 1) / 100
+        + (y - 1) / 400
+        + endDate.getDayofYear();
+
+    return days2 - days1;
+}
+
+void Software::print()
+{
+    cout << "name:" << name << endl;
+
+    cout << "type:";
+    if (type == 'O')
+        cout << "original" << endl;
+    else if (type == 'T')
+        cout << "trial" << endl;
+    else if (type == 'B')
+        cout << "backup" << endl;
+
+    cout << "media:";
+    if (media == 'D')
+        cout << "optical disk" << endl;
+    else if (media == 'H')
+        cout << "hard disk" << endl;
+    else if (media == 'U')
+        cout << "USB disk" << endl;
+
+    if (endDate.getYear() == 0 && endDate.getMonth() == 0 && endDate.getDay() == 0)
+    {
+        cout << "this software has unlimited use" << endl;
+    }
+    else
+    {
+        int days = getRemainDays();
+
+        if (days < 0)
+            cout << "this software has expired" << endl;
+        else
+            cout << "this software is going to be expired in "
+            << days << " days" << endl;
+    }
+
+    cout << endl;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        char name[31];
+        char type, media;
+        int year, month, day;
+
+        cin >> name;
+        cin >> type >> media >> year >> month >> day;
+
+        Software s1(name, type, media, year, month, day);
+        Software s2(s1);
+
+        s1.print();
+        s2.print();
+    }
+
+    return 0;
+}
