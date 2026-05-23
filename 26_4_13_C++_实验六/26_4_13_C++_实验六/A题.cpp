09772193bd8264a7095567040638fedeff10e902
guise-zxy <genius200612zhxy@qq.com>
2026-05-23 11:03:59 +0800

    Add files via upload

---
+++ 26_4_13_C++_实验六/26_4_13_C++_实验六/A题.cpp
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+#include<cstring>
+using namespace std;
+class stu 
+{
+	private:
+		stu* p;
+		string id;
+		char sex;
+		string college;
+		string phone;
+
+
+	public:
+
+};
+
+int main()
+{
+	int t = 0;
+	scanf("%d", &t);
+	while(t--)
+	{ 
+		cin >> p;
+	}
+
+
+
+
+	return 0;
+}
